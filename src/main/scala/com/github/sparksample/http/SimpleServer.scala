55faddde2509caa2cd6d02f2bf0ae13b26b89ec7
dnaumenko <dm.naumenko@gmail.com>
2017-04-21 17:07:41 +0300

    add retention calculation example with join

--- src/main/scala/com/github/sparksample/http/SimpleServer.scala
+++ src/main/scala/com/github/sparksample/http/SimpleServer.scala
@@ -6,12 +6,12 @@ import akka.http.scaladsl.server.{Directives, Route}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
-import org.apache.spark.SparkContext
 import spray.json._
 
 import scala.concurrent.Future
-import com.datastax.spark.connector._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.DataTypes
 
 final case class Identity(id: Long)
 
@@ -36,15 +36,34 @@ object SimpleServer extends Directives with JsonSupport {
         val events = ssc.read
           .format("org.apache.spark.sql.cassandra").options(Map("keyspace" -> "spark_test", "table" -> "events"))
           .load()
+          .where("event_id == 4")
 
         val devices = ssc.read
           .format("org.apache.spark.sql.cassandra").options(Map("keyspace" -> "spark_test", "table" -> "devices"))
           .load()
 
-        val join = events.join(devices, "device_id")
+        val join = events
+          .join(devices, Seq("device_id"), "left")
+          .withColumn("install_date", col("received_at").cast(DataTypes.DateType))
+          .withColumn("event_date", col("event_at").cast(DataTypes.DateType))
+          .withColumn("d1_retention", when(col("event_date") === date_add(col("install_date"), 1), 1).otherwise(0))
+          .withColumn("d7_retention", when(col("event_date") === date_add(col("install_date"), 7), 1).otherwise(0))
+          .withColumn("d15_retention", when(col("event_date") === date_add(col("install_date"), 15), 1).otherwise(0))
+          .withColumn("d30_retention", when(col("event_date") === date_add(col("install_date"), 30), 1).otherwise(0))
 
-        join.show()
-        complete("TODO")
+        join.persist()
+
+        val j = join.select("*")
+          .groupBy("platform_name", "language", "device_model", "ip_country", "app_version", "os_version", "install_date")
+          .agg(
+            count("id").alias("loggin_count")
+            , sum("d1_retention").alias("d1_retention")
+            , sum("d7_retention").alias("d7_retention")
+            , sum("d15_retention").alias("d15_retention")
+            , sum("d30_retention").alias("d30_retention"))
+
+        j.show()
+        complete("Check logs")
       }
     }
   }
